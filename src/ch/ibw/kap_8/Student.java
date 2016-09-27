5549f795054814ee7bf1a18ac8df5849f177a11c
hansjuergnett <goeginett@hotmail.com>
2016-09-27 21:14:28 +0200

    Klasse Student ausgeweitet

--- src/ch/ibw/kap_8/Student.java
+++ src/ch/ibw/kap_8/Student.java
@@ -1,34 +1,173 @@
 package ch.ibw.kap_8;
 
 public class Student {
-	
+
+	/* ==========
+     KONSTANTEN
+     ==========
+   */
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Mathematik */
+	public static final int MATHEMATIKSTUDIUM = 1;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Informatik */
+	public static final int INFORMATIKSTUDIUM = 2;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Architektur */
+	public static final int ARCHITEKTURSTUDIUM = 3;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 der Wirtschaftswissenschaften */
+	public static final int WIRTSCHAFTLICHESSTUDIUM = 4;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Biologie */
+	public static final int BIOLOGIESTUDIUM = 5;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Geschichte */
+	public static final int GESCHICHTSSTUDIUM = 6;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Germanistik */
+	public static final int GERMANISTIKSTUDIUM = 7;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Politologie */
+	public static final int POLITOLOGIESTUDIUM = 8;
+
+	/** Diese Konstante symbolisiert das Studienfach
+	 Physik */
+	public static final int PHYSIKSTUDIUM = 9;
+
+    public static final int WEIBLICH = 100;
+    public static final int MAENNLICH = 101;
+
+	private static int zaehler = 0;
 	private String name;
 	private int nummer;
-	
-	public String getName() {
+	private int fach;
+    private int geburtsjahr;
+    private int geschlecht;
+
+    public Student() {
+        this(1970);
+    }
+
+    public Student(int geburtsjahr) {
+        this.geburtsjahr = geburtsjahr;
+        zaehler++;
+    }
+
+    public Student(String name, int nummer, int fach, int geburtsjahr) {
+        this(geburtsjahr);
+        this.name = name;
+        this.nummer = nummer;
+        this.fach = fach;
+    }
+
+    public Student(String name, int nummer, int fach, int geburtsjahr, int geschlecht) {
+        this(geburtsjahr);
+        this.name = name;
+        this.nummer = nummer;
+        this.fach = fach;
+        this.geschlecht = geschlecht;
+    }
+
+    public static int getZaehler() {
+        return zaehler;
+    }
+
+    public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getNummer() {
 		return nummer;
 	}
+
 	public void setNummer(int nummer) {
 		int alteNummer = this.nummer;
 		this.nummer = nummer;
-		if(!valideteNummer()){
+		if(!validateNummer()){
 			this.nummer = alteNummer;
-		}		
-		this.nummer = nummer;
+		}
 	}
 
-	public boolean valideteNummer(){
+	public boolean validateNummer() {
 		return ((nummer >= 10000) && (nummer <= 99999) && (nummer % 2 != 0));
 	}
-	@Override
+
+	public int getFach() {
+		return fach;
+	}
+
+	public void setFach(int fach) {
+		this.fach = fach;
+	}
+
+    public int getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setGeschlecht(int geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+
+    @Override
 	public String toString() {
-		return name + " (" + nummer + ")"; 
+        String res = name + " (" + nummer + ")";
+
+        switch(geschlecht){
+            case WEIBLICH:
+                res += " ist weiblich und";
+                break;
+            case MAENNLICH:
+                res += " ist männlich und ";
+                break;
+            default:
+                res += " hat kein definiertes Geschlecht ";
+        }
+
+		switch (fach){
+			case MATHEMATIKSTUDIUM:
+			    res += " ein Mathematikstudent";
+				break;
+            case INFORMATIKSTUDIUM:
+                res += " ein Informatikstudent";
+                break;
+            case ARCHITEKTURSTUDIUM:
+                res += " angehender Architekt.";
+                break;
+            case WIRTSCHAFTLICHESSTUDIUM:
+                res += " ein Wirtschaftswissenschaftler.";
+                break;
+            case BIOLOGIESTUDIUM:
+                res += " Biologie ist seine Staerke.";
+                break;
+            case GESCHICHTSSTUDIUM:
+                res += " sollte Geschichte nicht mit Geschichten verwechseln.";
+                break;
+            case GERMANISTIKSTUDIUM:
+                res += " wird einmal Germanist gewesen geworden sein.";
+                break;
+            case POLITOLOGIESTUDIUM:
+                res += " kommt bestimmt einmal in den Bundestag.";
+                break;
+            case PHYSIKSTUDIUM:
+                res += " studiert schon relativ lange Physik.";
+                break;
+            default:
+                res += " keine Angabe zum Studium";
+		}
+		return res;
 	}
 	
 	
--- src/ch/ibw/kap_8/TestStudent.java
+++ src/ch/ibw/kap_8/TestStudent.java
@@ -6,9 +6,25 @@ public class TestStudent {
 		
 		Student studi1 = new Student();
 		studi1.setName("Meier");
-		studi1.setNummer(15);
-		
-		System.out.println(studi1);
-	}
+		studi1.setNummer(70615);
+        studi1.setFach(Student.BIOLOGIESTUDIUM);
+        studi1.setGeschlecht(Student.MAENNLICH);
+        System.out.println(studi1);
+
+        Student studi3 = new Student(1985);
+        studi3.setName("Müller");
+        studi3.setNummer(19783);
+        studi3.setFach(Student.INFORMATIKSTUDIUM);
+        studi3.setGeschlecht(Student.WEIBLICH);
+        System.out.println(studi3);
+
+        Student studi2 = new Student("Huber",15315, Student.MATHEMATIKSTUDIUM, 1968);
+        System.out.println(studi2);
+
+        Student studi4 = new Student("Berger", 62871, Student.PHYSIKSTUDIUM,1988, Student.WEIBLICH);
+        System.out.println(studi4);
+
+        System.out.println("Anzahl Studenten: " + Student.getZaehler());
+    }
 
 }

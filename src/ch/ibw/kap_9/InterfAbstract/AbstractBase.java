e7e7e8033ea4c5ba8ba141febaecd4ec2cfd78eb
hansjuergnett <goeginett@hotmail.com>
2016-10-12 21:17:54 +0200

    Uebungen aktualisiert

---
+++ src/ch/ibw/kap_9/InterfAbstract/AbstractBase.java
@@ -0,0 +1,13 @@
+package ch.ibw.kap_9.InterfAbstract;
+
+/**
+ * Created by Nett on 11.10.2016.
+ */
+public abstract class AbstractBase {
+
+    public  abstract void  print();
+
+    public void sayHallo(){
+        System.out.println("Hallo!");
+    }
+}
